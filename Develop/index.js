c1379ac1a29746767342edc33e0aa968c95c0fce
Xandi <santosal2018@gmail.com>
2023-08-06 11:05:17 -0400

    add view employees function

--- Develop/index.js
+++ Develop/index.js
@@ -128,3 +128,15 @@ function loadMainPrompts() {
     }
     )
   }
+
+  // View all employees
+function viewEmployees() {
+    db.findAllEmployees()
+      .then(([rows]) => {
+        let employees = rows;
+        console.log("\n");
+        console.table(employees);
+      })
+      .then(() => loadMainPrompts());
+  }
+  
